e1450379b6de621fcd3ac70d643d5bb1c8e7a676
Chester Southwood <csouthwood@eagles.ewu.edu>
2019-08-09 22:18:09 -0700

    Fixed path error, correctly connected to manifest.json. (Cont.)
    Look at manifest.json to edit values, init.js follows at the start of the program that checks values in manifest.json to use alternative_css and or custom css values in manifest.json.

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ function loadJSON(callback) {
     let xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     //will need to change path
-    xobj.open('GET', '/CSCD378-FinalProject-STABUS/manifest.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', './styles/manifest.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
