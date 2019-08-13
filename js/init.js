3975ec4c2097a568e7a5e95ea01fde20bde332aa
Matthew Jett <matthew.jett@outlook.com>
2019-08-13 11:08:18 -0700

    simple formatting, nothing major

--- js/init.js
+++ js/init.js
@@ -13,12 +13,11 @@ function loadJSON(callback) {
     xobj.send(null);  
  }
  
- function init() {
-       loadJSON(function(response) {
-       let actual_JSON = JSON.parse(response);
-
-       //check if they want to edit through the manifest.json for badging
-       if(actual_JSON.settings.badging_css == "true") {
+function init() {
+    loadJSON(function(response) {
+    let actual_JSON = JSON.parse(response);
+        //check if they want to edit through the manifest.json for badging
+        if(actual_JSON.settings.badging_css == "true") {
             console.log(`element => property : value`);
             for(let x in actual_JSON.settings.manual_custom_css){
                 for(let y in actual_JSON.settings.manual_custom_css[x]){
